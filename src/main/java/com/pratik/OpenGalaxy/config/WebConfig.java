dccd9556a8659360ea7031264603e141351e3a34
Ishan M <imamidpalliwar@gmail.com>
2025-08-31 23:07:27 +0530

    Your commit message here

---
+++ src/main/java/com/pratik/OpenGalaxy/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.pratik.OpenGalaxy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;  // e
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // allow frontend React app
+                        .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*") // allow all headers
+                        .allowCredentials(true); // 🔑 allow cookies (JSESSIONID)
+            }
+        };
+    }
+}
